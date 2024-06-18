375b04ebc156d1b3fa0ac53d2de53f5b7a5d4eed
AlonaTutors <alona.chubenko@tutors.es>
2024-06-18 10:32:43 +0300

    Add colors to tailwind config

--- ts-matkalasku-app/tailwind.config.js
+++ ts-matkalasku-app/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Dark mode
+        "darkestBlue-dark": "#1E1F26",
+        "background-blue-dark": "#283655",
+        "blueberry-dark": "#4D648D",
+        "skyBlue-dark": "#D0E1F9",
+        // Light mode
+        "background-lightGray": "#F1F1F2",
+        "grayishCyan-light": "#F5FDFC",
+        "glacierBlue-light": "#1995AD",
+        "iceColor-light": "#A1D6E2",
+        "warmGrayText-light": "#BCBABE",
+        "darkGrayText-light": "#444444",
+      }
+    },
   },
   plugins: [],
 }
